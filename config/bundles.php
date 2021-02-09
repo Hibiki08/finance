67c5d87de153ac2f39990556e45fb3c4c9daf708
Dmitrieva.GG <Dmitrieva.GG@parama.ru>
2021-02-09 22:18:15 +0300

    добавление doctrine orm

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
